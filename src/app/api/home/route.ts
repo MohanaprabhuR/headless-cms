1e5d1b4dc3f24f943c29291e441f40c1cbb3a4a4
Mohanaprabhu.R <mohan@timeless.co>
2025-05-29 16:23:48 +0530

    Inital commit for headless cms

---
+++ src/app/api/home/route.ts
@@ -0,0 +1,46 @@
+// app/api/home/route.ts
+
+import { NextResponse } from "next/server";
+import { gql, request } from "graphql-request";
+
+const QUERY = gql`
+  query GetAllPages {
+    pages(first: 100) {
+      edges {
+        node {
+          id
+          title
+          content
+          slug
+          date
+          uri
+        }
+      }
+    }
+  }
+`;
+
+export async function GET() {
+  const API_URL = process.env.API_URL;
+  if (!API_URL) {
+    console.error("Missing API_URL in environment variables");
+    return NextResponse.json(
+      { error: "API_URL is not defined in the environment" },
+      { status: 500 }
+    );
+  }
+
+  try {
+    const data = await request(`${API_URL}/graphql`, QUERY);
+    return NextResponse.json(data);
+  } catch (error: any) {
+    console.error("GraphQL request error:", error);
+    return NextResponse.json(
+      {
+        error: "Failed to fetch data from WordPress GraphQL API",
+        details: error.response?.errors ?? error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,73 @@
+// app/api/home/route.ts
+
+import { NextResponse } from "next/server";
+import { gql, request } from "graphql-request";
+
+const QUERY = gql`
+  {
+    posts(first: 100) {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          content
+          date
+          slug
+          author {
+            node {
+              id
+              name
+            }
+          }
+          featuredImage {
+            node {
+              sourceUrl
+            }
+          }
+          categories {
+            edges {
+              node {
+                name
+                slug
+              }
+            }
+          }
+          tags {
+            edges {
+              node {
+                tagId
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export async function GET() {
+  const API_URL = process.env.API_URL;
+  if (!API_URL) {
+    console.error("Missing API_URL in environment variables");
+    return NextResponse.json(
+      { error: "API_URL is not defined in the environment" },
+      { status: 500 }
+    );
+  }
+
+  try {
+    const data = await request(`${API_URL}/graphql`, QUERY);
+    return NextResponse.json(data);
+  } catch (error: any) {
+    console.error("GraphQL request error:", error);
+    return NextResponse.json(
+      {
+        error: "Failed to fetch data from WordPress GraphQL API",
+        details: error.response?.errors ?? error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
